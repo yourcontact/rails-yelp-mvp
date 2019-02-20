c7722996150dba252056783dec38eced401f6e81
Romain Aubert <rpsa@pm.me>
2019-02-20 15:38:21 +0000

    create one restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Restaurant.create.new for each do
+#   faker with name address category reviews
+
+restaurant = Restaurant.create(name: "dim sum ngmaisai", address: "TST", phone_number: "85223452345", category: "chinese")
+review = Review.new(content: "mama ruru", rating: 5)
+review.restaurant = restaurant
+review.save
